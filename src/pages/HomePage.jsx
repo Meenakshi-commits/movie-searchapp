3a62ef4d4b6ce89b56e2863896c5503223a6d0ed
Meenakshi-commits <meenakshi.v36@gmail.com>
2025-02-03 23:13:44 +0530

    Fixed case sensitivity issue with components folder

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { fetchMovies } from '../services/omdbApi';
-import MovieCard from '../components_temp/MovieCard';
+import MovieCard from '../components/MovieCard';
 
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
